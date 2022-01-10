bee34bee9738ef7717175fafc06270a0a3b76329
alexandershearer <alex.shearer23@gmail.com>
2022-01-10 03:04:51 -0600

    Fixed a logo, got some social icons, and adding buttons to navigate to different pages

---
+++ src/subComponents/SocialIcons.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import { Github, Twitter } from '../components/AllSvgs'
+
+
+const Icons = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+position:fixed;
+bottom: 0;
+left: 2rem;
+
+z-index:3;
+
+&>*:not(:last-child){
+    margin: 0.5rem 0;
+}
+`
+
+const Line = styled.span`
+width: 2px;
+height: 8rem;
+background-color: ${props => props.theme.text}
+`
+
+const SocialIcons = () => {
+    return (
+        <Icons>
+            <div>
+                <NavLink style={{ color: 'inherit' }} target="_blank" to="/">
+                    <Github width={25} height={25} fill='currentColor' />
+                </NavLink>
+            </div>
+            <div>
+                <NavLink style={{ color: 'inherit' }} target="_blank" to="/">
+                    <Twitter width={25} height={25} fill='currentColor' />
+                </NavLink>
+            </div>
+            <div>
+                <NavLink style={{ color: 'inherit' }} target="_blank" to="/">
+                    <Twitter width={25} height={25} fill='currentColor' />
+                </NavLink>
+            </div>
+            <div>
+                <NavLink style={{ color: 'inherit' }} target="_blank" to="/">
+                    <Twitter width={25} height={25} fill='currentColor' />
+                </NavLink>
+            </div>
+
+            <Line></Line>
+        </Icons>
+    )
+}
+
+export default SocialIcons
