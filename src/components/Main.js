bfa9ebd887e5ac8770e70e36525e3c7966f39037
alexandershearer <alex.shearer23@gmail.com>
2022-01-10 02:11:57 -0600

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const MainContainer = styled.div`
+background: ${props => props.theme.body}:
+`
+
+const Main = () => {
+    return (
+        <MainContainer>
+            Main Component/Page
+        </MainContainer>
+    )
+}
+
+export default Main
