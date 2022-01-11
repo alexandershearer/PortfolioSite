0ec19126eefc74e8c6711aa84960865c9077a45c
alexandershearer <alex.shearer23@gmail.com>
2022-01-10 22:14:24 -0600

    Finished about page with simple text and description with animated background js library

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider, keyframes } from 'styled-components'
 import { DarkTheme } from './Themes'
 
 
@@ -19,11 +19,24 @@ position: relative;
 overflow: hidden;
 `
 
+const float = keyframes`
+0% {
+    transform: translateY(-10px) 
+}
+50% {
+    transform: translateY(15px) translateX(15px) 
+}
+100% {
+    transform: translateY(-10px) 
+}
+`
+
 const Spaceman = styled.div`
 position: absolute;
-top: 10%;
+top: 20%;
 right: 5%;
 width: 20vw;
+animation: ${float} 4s ease infinite;
 
 img{
     width: 100%;
@@ -44,7 +57,7 @@ display:flex;
 justify-content-center;
 align-items: center;
 font-size: calc(0.6rem + 1vw);
-backdrop-filter:blur(4px);
+backdrop-filter: blur(5px);
 
 position: absolute;
 left: calc(5rem + 5vw);
@@ -72,6 +85,16 @@ const AboutPage = () => {
                 <Spaceman>
                     <img src={astronaut} alt="spaceman" />
                 </Spaceman>
+
+                <Main>
+                    I am a front-end developer from Kentucky, USA. I enjoy making simple websites that the user can enjoy.
+                    <br />
+                    <br />
+                    I am interested in frontend stack while trying new things and making new projects. I'm currently in school finishing up a Bachelors degree in Applied Computer Science. I like to watch movies and cook occasionally.
+                    <br />
+                    <br />
+                    As long as it's a passion and you give it your all then nothing can stop you from being great at what you do.
+                </Main>
             </Box>
 
         </ThemeProvider>
