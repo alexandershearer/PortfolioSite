74af7d750701b6e5ee9d352f8a0fecc1e5d90da5
alexandershearer <alex.shearer23@gmail.com>
2022-01-14 15:37:07 -0600

    Fixing social links

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -29,17 +29,17 @@ const SocialIcons = (props) => {
     return (
         <Icons>
             <div>
-                <NavLink style={{ color: 'inherit' }} target="_blank" to="https://github.com/alexandershearer">
+                <NavLink style={{ color: 'inherit' }} target="_blank" to={{ pathname: "https://github.com/alexandershearer" }}>
                     <Github width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body} />
                 </NavLink>
             </div>
             <div>
-                <NavLink style={{ color: 'inherit' }} target="_blank" to="https://twitter.com/nuveious">
+                <NavLink style={{ color: 'inherit' }} target="_blank" to={{ pathname: "https://twitter.com/nuveious" }}>
                     <Twitter width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body} />
                 </NavLink>
             </div>
             <div>
-                <NavLink style={{ color: 'inherit' }} target="_blank" to="https://www.instagram.com/nuvelous/">
+                <NavLink style={{ color: 'inherit' }} target="_blank" to={{ pathname: "https://www.instagram.com/nuvelous/" }}>
                     <Instagram width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body} />
                 </NavLink>
             </div>
